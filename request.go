d952d7250cf011f61790719ae1f9e0fab4836da2
Charlie Wu <wu@fox.one>
2019-01-08 12:17:34 +0800

    MOD: add GenerateToken;

--- request.go
+++ request.go
@@ -10,6 +10,27 @@ import (
 	"github.com/fox-one/mixin-sdk/utils"
 )
 
+// GenerateToken generate jwt token
+func (broker *Broker) GenerateToken(userID, pinToken, nonce string) (string, error) {
+	jwtMap := map[string]interface{}{
+		"i": broker.BrokerID,
+	}
+	if len(userID) > 0 {
+		jwtMap["u"] = userID
+	}
+
+	if len(pinToken) > 0 {
+		jwtMap["pt"] = pinToken
+	}
+
+	token, err := broker.Sign(jwtMap, time.Now().Unix()+60, nonce)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
 // Request sign and request
 //  jwt token, {"i":"broker-id","u":"user-id","n":"nonce","e":123,"nr":2,"pt":"pin token"}
 func (broker *Broker) Request(ctx context.Context, userID string, method, uri string, params map[string]interface{}, headers ...string) ([]byte, error) {
@@ -36,18 +57,7 @@ func (broker *Broker) RequestWithPIN(ctx context.Context, userID, pinToken, nonc
 		body = b
 	}
 
-	jwtMap := map[string]interface{}{
-		"i": broker.BrokerID,
-	}
-	if len(userID) > 0 {
-		jwtMap["u"] = userID
-	}
-
-	if len(pinToken) > 0 {
-		jwtMap["pt"] = pinToken
-	}
-
-	token, err := broker.Sign(jwtMap, time.Now().Unix()+60, nonce)
+	token, err := broker.GenerateToken(userID, pinToken, nonce)
 	if err != nil {
 		return nil, err
 	}
